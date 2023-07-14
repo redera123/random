bf74f9b8776254179952019661d402313eacbc04
redera123 <139486417+redera123@users.noreply.github.com>
2023-07-14 14:03:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('количество паролей?: ')
+length = input('длина пароля?: ')
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print(password)
